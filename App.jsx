512f445379d7b2e8a7728d63ff64f1e998f5c333
tyupi7 <87684919+tyupi7@users.noreply.github.com>
2021-08-03 17:19:42 +0900

    AvoidingView -> SafeView

--- App.jsx
+++ App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 // import MemoListScreen from './src/screen/MemoListScreen';
 // import MemoDetailScreen from './src/screen/MemoDetail';
-// import MemoEditScreen from './src/screen/MemoEditScreen';
-import MemoCreateScreen from './src/screen/MemoCreateScreen';
+import MemoEditScreen from './src/screen/MemoEditScreen';
+// import MemoCreateScreen from './src/screen/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoCreateScreen />
+    <MemoEditScreen />
   );
 }
--- src/screen/MemoEditScreen.jsx
+++ src/screen/MemoEditScreen.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import {
-  View, TextInput, StyleSheet, KeyboardAvoidingView,
+  View, TextInput, StyleSheet,
 } from 'react-native';
 
 import AppBar from '../components/AppBar';
 import CircleBottun from '../components/CircleBottun';
+import KeyboardSafeView from '../components/KeyboardAView';
 
 export default function MemoEditScreen() {
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="height">
+    <KeyboardSafeView style={styles.container}>
       <AppBar />
       <View style={styles.inputContainer}>
         <TextInput value="買い物リスト" multiline style={styles.input} />
       </View>
       <CircleBottun name="check" />
-    </KeyboardAvoidingView>
+    </KeyboardSafeView>
   );
 }
 
