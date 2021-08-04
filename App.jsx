679e2818798a5597b58c455fe6189ad3adc8eb82
tyupi7 <87684919+tyupi7@users.noreply.github.com>
2021-08-04 10:13:15 +0900

    Create Enter Screem

--- App.jsx
+++ App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 // import MemoListScreen from './src/screen/MemoListScreen';
 // import MemoDetailScreen from './src/screen/MemoDetail';
-import MemoEditScreen from './src/screen/MemoEditScreen';
+// import MemoEditScreen from './src/screen/MemoEditScreen';
 // import MemoCreateScreen from './src/screen/MemoCreateScreen';
+// import LogInScreen from './src/screen/LogInScreen';
+import SignUpScreen from './src/screen/SignUpScreen';
 
 export default function App() {
   return (
-    <MemoEditScreen />
+    <SignUpScreen />
   );
 }
