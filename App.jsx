20ef8b405d1f711c378013b96708b4c93f6ed240
tyupi7 <87684919+tyupi7@users.noreply.github.com>
2021-08-04 11:14:59 +0900

    Touchable

--- App.jsx
+++ App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-// import MemoListScreen from './src/screen/MemoListScreen';
+import MemoListScreen from './src/screen/MemoListScreen';
 // import MemoDetailScreen from './src/screen/MemoDetail';
 // import MemoEditScreen from './src/screen/MemoEditScreen';
 // import MemoCreateScreen from './src/screen/MemoCreateScreen';
 // import LogInScreen from './src/screen/LogInScreen';
-import SignUpScreen from './src/screen/SignUpScreen';
+// import SignUpScreen from './src/screen/SignUpScreen';
 
 export default function App() {
   return (
-    <SignUpScreen />
+    <MemoListScreen />
   );
 }
--- src/components/CircleBottun.jsx
+++ src/components/CircleBottun.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { string, shape } from 'prop-types';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { string, shape, func } from 'prop-types';
 import { Feather } from '@expo/vector-icons';
 
 export default function CircleBottun(props) {
-  const { style, name } = props;
+  const { style, name, onPress } = props;
   return (
-    <View style={[styles.circleBottun, style]}>
+    <TouchableOpacity style={[styles.circleBottun, style]} onPress={onPress}>
       <Feather name={name} size={32} color="white" />
-    </View>
+    </TouchableOpacity>
   );
 }
 
 CircleBottun.propTypes = {
   style: shape(),
   name: string.isRequired,
+  onPress: func,
 };
 
 CircleBottun.defaultProps = {
   style: null,
+  onPress: null,
 };
 
 const styles = StyleSheet.create({
