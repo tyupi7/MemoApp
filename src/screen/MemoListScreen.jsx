07771cb237f20c81b6326d96e645afa5e763681b
tyupi7 <87684919+tyupi7@users.noreply.github.com>
2021-08-05 18:12:47 +0900

    Log Out

--- src/screen/MemoListScreen.jsx
+++ src/screen/MemoListScreen.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import MemoList from '../components/MemoList';
 import CircleBottun from '../components/CircleBottun';
+import LogOutButton from '../components/LogOutButton';
 
 export default function MemoListScreen(prop) {
   const { navigation } = prop;
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <LogOutButton />,
+    });
+  }, []);
   return (
     <View style={styles.container}>
       <MemoList />
