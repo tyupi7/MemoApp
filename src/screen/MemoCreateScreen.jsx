32e6c4752070bdb6a84819a6c71141f85bba3794
tyupi7 <87684919+tyupi7@users.noreply.github.com>
2021-08-06 11:42:16 +0900

    DB Update

--- src/screen/MemoCreateScreen.jsx
+++ src/screen/MemoCreateScreen.jsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View, TextInput, StyleSheet,
 } from 'react-native';
+
+import firebase from 'firebase';
 
 import CircleBottun from '../components/CircleBottun';
 import KeyboardSafeView from '../components/KeyboardAView';
 
 export default function MemoCreateScreen(props) {
   const { navigation } = props;
+  const [bodyText, setBodyText] = useState('');
+
+  function handlePress() {
+    const { currentUser } = firebase.auth();
+    const db = firebase.firestore();
+    const ref = db.collection(`users/${currentUser.uid}/memos`);
+    ref.add({
+      bodyText, // キーと値が同じ場合は省略して一つにかける 本来はbody: body,
+      updatedAt: new Date(),
+    })
+      .then((docRef) => {
+        console.log('Created', docRef.id);
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.log('Error', error);
+      });
+  }
+
   return (
     <KeyboardSafeView style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput value="" multiline style={styles.input} />
+        <TextInput
+          value={bodyText}
+          multiline
+          style={styles.input}
+          onChangeText={(text) => { setBodyText(text); }}
+          autoFocus
+        />
       </View>
       <CircleBottun
         name="check"
-        onPress={() => { navigation.goBack(); }}
+        onPress={handlePress}
       />
     </KeyboardSafeView>
   );
